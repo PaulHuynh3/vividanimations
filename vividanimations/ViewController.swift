23ff2d2e60f357204b10bbfe287993225f7f47c0
Paul Huynh <paul.huynh@aircanada.ca>
2020-08-22 23:31:09 -0400

    Initial setup. Set up stackviews to display first view controller.

---
+++ vividanimations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VividAnimations
+//
+//  Created by Paul Huynh on 2020-08-22.
+//  Copyright © 2020 PXSDevelopment. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //set up UI
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .yellow
+    }
+
+}
+
