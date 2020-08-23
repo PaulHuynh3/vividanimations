1f34622a68f1d6d7c0f13dd0a1c0cdc8eec44984
Paul Huynh <paul.huynh@aircanada.ca>
2020-08-23 01:17:52 -0400

    added chain animation when tapped on screen to make title label and description label disappear

---
+++ vividanimations/FeedbackViewController.swift
@@ -0,0 +1,57 @@
+//
+//  FeedbackViewController.swift
+//  VividAnimations
+//
+//  Created by Paul Huynh on 2020-08-23.
+//  Copyright © 2020 PXSDevelopment. All rights reserved.
+//
+
+import UIKit
+
+class FeedbackViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pageControl.pageIndicatorTintColor = .red
+        pageControl.currentPageIndicatorTintColor = .blue
+    }
+
+    @IBAction func viewDidTapped(_ sender: Any) {
+
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+
+            self.titleLabel.transform = CGAffineTransform(translationX: -50, y: 0)
+
+
+        }) { (_) in
+
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+
+                self.titleLabel.alpha = 0
+                self.titleLabel.transform = self.titleLabel.transform.translatedBy(x: 0, y: -200)
+
+            })
+        }
+
+        UIView.animate(withDuration: 0.5, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+
+            self.descriptionLabel.transform = CGAffineTransform(translationX: -50, y: 0)
+
+
+        }) { (_) in
+
+            UIView.animate(withDuration: 0.5, delay: 0.5, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+
+                self.descriptionLabel.alpha = 0
+                self.descriptionLabel.transform = self.titleLabel.transform.translatedBy(x: 0, y: -200)
+
+            })
+        }
+
+    }
+}
